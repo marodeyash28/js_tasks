fde5f530c605d5e0ea28636d515ba51514fc0a6a
Yash Marode <123548505+marodeyash28@users.noreply.github.com>
2024-07-03 14:09:11 +0530

    Add files via upload

---
+++ node/dateMethods/dateMethods.js
@@ -0,0 +1,332 @@
+// 1. Date.prototype.getDate()
+const getDateExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.getDate());
+}
+
+// 2. Date.prototype.getDay()
+const getDayExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.getDay());
+}
+
+// 3. Date.prototype.getFullYear()
+const getFullYearExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.getFullYear()); 
+}
+
+// 4. Date.prototype.getHours()
+const getHoursExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.getHours());
+}
+
+// 5. Date.prototype.getMilliseconds()
+const getMillisecondsExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.getMilliseconds());
+}
+
+// 6. Date.prototype.getMinutes()
+const getMinutesExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.getMinutes());
+}
+
+// 7. Date.prototype.getMonth()
+const getMonthExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.getMonth());
+}
+
+// 8. Date.prototype.getSeconds()
+const getSecondsExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.getSeconds());
+}
+
+// 9. Date.prototype.getTime()
+const getTimeExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.getTime());
+}
+
+// 10. Date.prototype.getTimezoneOffset()
+const getTimezoneOffsetExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.getTimezoneOffset());
+}
+
+// 11. Date.prototype.getUTCDate()
+const getUTCDateExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.getUTCDate()); 
+}
+
+// 12. Date.prototype.getUTCDay()
+const getUTCDayExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.getUTCDay());
+}
+
+// 13. Date.prototype.getUTCFullYear()
+const getUTCFullYearExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.getUTCFullYear());
+}
+
+// 14. Date.prototype.getUTCHours()
+const getUTCHoursExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.getUTCHours());
+}
+
+// 15. Date.prototype.getUTCMilliseconds()
+const getUTCMillisecondsExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.getUTCMilliseconds());
+}
+
+// 16. Date.prototype.getUTCMinutes()
+const getUTCMinutesExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.getUTCMinutes());
+}
+
+// 17. Date.prototype.getUTCMonth()
+const getUTCMonthExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.getUTCMonth()); 
+}
+
+// 18. Date.prototype.getUTCSeconds()
+const getUTCSecondsExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.getUTCSeconds());
+}
+
+// 19. Date.prototype.getYear() (Deprecated)
+const getYearExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.getYear());
+}
+
+// 20. Date.prototype.setDate()
+const setDateExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    bday.setDate(15);
+    console.log(bday);
+}
+
+// 21. Date.prototype.setFullYear()
+const setFullYearExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    bday.setFullYear(2020);
+    console.log(bday);
+}
+
+// 22. Date.prototype.setHours()
+const setHoursExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    bday.setHours(20);
+    console.log(bday);
+}
+
+// 23. Date.prototype.setMilliseconds()
+const setMillisecondsExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    bday.setMilliseconds(500);
+    console.log(bday);
+}
+
+// 24. Date.prototype.setMinutes()
+const setMinutesExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    bday.setMinutes(45);
+    console.log(bday);
+}
+
+// 25. Date.prototype.setMonth()
+const setMonthExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    bday.setMonth(5);
+    console.log(bday); 
+}
+
+// 26. Date.prototype.setSeconds()
+const setSecondsExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    bday.setSeconds(45);
+    console.log(bday);
+}
+
+// 27. Date.prototype.setTime()
+const setTimeExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    bday.setTime(1609459200000);
+    console.log(bday); 
+}
+
+// 28. Date.prototype.setUTCDate()
+const setUTCDateExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    bday.setUTCDate(15);
+    console.log(bday);
+}
+
+// 29. Date.prototype.setUTCFullYear()
+const setUTCFullYearExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    bday.setUTCFullYear(2020);
+    console.log(bday);
+}
+
+// 30. Date.prototype.setUTCHours()
+const setUTCHoursExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    bday.setUTCHours(20);
+    console.log(bday);
+}
+
+// 31. Date.prototype.setUTCMilliseconds()
+const setUTCMillisecondsExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    bday.setUTCMilliseconds(500);
+    console.log(bday);
+}
+
+// 32. Date.prototype.setUTCMinutes()
+const setUTCMinutesExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    bday.setUTCMinutes(45);
+    console.log(bday);
+}
+
+// 33. Date.prototype.setUTCMonth()
+const setUTCMonthExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    bday.setUTCMonth(5);
+    console.log(bday); 
+}
+
+// 34. Date.prototype.setUTCSeconds()
+const setUTCSecondsExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    bday.setUTCSeconds(45);
+    console.log(bday); 
+}
+
+// 35. Date.prototype.setYear() (Deprecated)
+const setYearExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    bday.setYear(102);
+    console.log(bday);
+}
+
+// 36. Date.prototype.toDateString()
+const toDateStringExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.toDateString()); 
+}
+
+// 37. Date.prototype.toISOString()
+const toISOStringExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.toISOString()); 
+}
+
+// 38. Date.prototype.toJSON()
+const toJSONExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.toJSON()); 
+}
+
+// 39. Date.prototype.toLocaleDateString()
+const toLocaleDateStringExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.toLocaleDateString()); 
+}
+
+// 40. Date.prototype.toLocaleString()
+const toLocaleStringExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.toLocaleString()); 
+}
+
+// 41. Date.prototype.toLocaleTimeString()
+const toLocaleTimeStringExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.toLocaleTimeString());
+}
+
+// 42. Date.prototype.toString()
+const toStringExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.toString()); 
+}
+
+// 43. Date.prototype.toTimeString()
+const toTimeStringExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.toTimeString()); 
+}
+
+// 44. Date.prototype.toUTCString()
+const toUTCStringExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.toUTCString()); 
+}
+
+// 45. Date.prototype.valueOf()
+const valueOfExample = () => {
+    const bday = new Date('December 28, 2002 15:29:30');
+    console.log(bday.valueOf()); 
+}
+
+// Call each function
+getDateExample();
+getDayExample();
+getFullYearExample();
+getHoursExample();
+getMillisecondsExample();
+getMinutesExample();
+getMonthExample();
+getSecondsExample();
+getTimeExample();
+getTimezoneOffsetExample();
+getUTCDateExample();
+getUTCDayExample();
+getUTCFullYearExample();
+getUTCHoursExample();
+getUTCMillisecondsExample();
+getUTCMinutesExample();
+getUTCMonthExample();
+getUTCSecondsExample();
+getYearExample();
+setDateExample();
+setFullYearExample();
+setHoursExample();
+setMillisecondsExample();
+setMinutesExample();
+setMonthExample();
+setSecondsExample();
+setTimeExample();
+setUTCDateExample();
+setUTCFullYearExample();
+setUTCHoursExample();
+setUTCMillisecondsExample();
+setUTCMinutesExample();
+setUTCMonthExample();
+setUTCSecondsExample();
+setYearExample();
+toDateStringExample();
+toISOStringExample();
+toJSONExample();
+toLocaleDateStringExample();
+toLocaleStringExample();
+toLocaleTimeStringExample();
+toStringExample();
+toTimeStringExample();
+toUTCStringExample();
+valueOfExample();
